7631d03824f00bbdfca86c57caf976ccda0ebdc3
Jhonata Galante <jhonata.galante@gmail.com>
2021-10-25 11:45:19 -0300

    Adicionar arquivos de projeto.

---
+++ DapperContribDemo/Entity/Menu.cs
@@ -0,0 +1,57 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DapperContribDemo.Entity
+{
+    [Table("Menu")]
+    public class Menu
+    {
+        private readonly string conn = "Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=teste;Integrated Security=True;Pooling=False";
+
+        [Key]
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedIn { get; set; }
+
+
+        //Read
+        public Menu Read(int id)
+        {
+            using(SqlConnection connection = new SqlConnection(conn))
+            {
+                return connection.Get<Menu>(id);
+            }
+        }
+
+        //Insert
+        public long Insert(Menu menu)
+        {
+            using (SqlConnection connection = new SqlConnection(conn))
+            {
+                return connection.Insert(menu);
+            }
+        }
+
+        //Update
+        public void Update(Menu menu)
+        {
+            using (SqlConnection connection = new SqlConnection(conn))
+            {
+                connection.Update(menu);
+            }
+        }
+
+        //Delete
+        public void Delete(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(conn))
+            {
+                connection.Delete(new Menu() { Id = id });
+            }
+        }
+    }
+}
---
+++ DapperContribDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using DapperContribDemo.Entity;
+using System;
+
+namespace DapperContribDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Insert
+            var insert = new Menu()
+            {
+                ProductId = 1,
+                Description = "Eggs",
+                CreatedIn = DateTime.UtcNow
+            };
+
+            insert.Insert(insert);
+            Console.WriteLine($"Inserted {insert.Description}");
+
+            //Get
+            var data = new Menu()
+            {
+                ProductId = insert.Id,
+            };
+            data.Read(insert.Id);
+
+            //Update
+            var update = new Menu()
+            {
+                ProductId = insert.Id,
+                Description = "Eggs and Bacon",
+                CreatedIn = DateTime.UtcNow
+            };
+            update.Update(update);
+            Console.WriteLine($"Updated {update.Description}");
+
+            //Delete
+            var delete = new Menu()
+            {
+                Id = insert.Id,
+            };
+            delete.Delete(delete.Id);
+            Console.WriteLine($"Deleted {delete.Id}");
+        }
+    }
+}
